f9f7a370a6f6892551bb6b21474824a9c6120031
João Vitor <151798489+joaogentelucio@users.noreply.github.com>
2024-12-02 19:28:02 -0300

    Versão funcional

--- src/DeployTeste/Program.cs
+++ src/DeployTeste/Program.cs
@@ -1,23 +1,8 @@
 ﻿using DeployTeste.Repositories;
 using FirebirdSql.Data.FirebirdClient;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Lê as variáveis de ambiente
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-var connectionString = Environment.GetEnvironmentVariable("FirebirdConnection");
-
-// Configura a string de conexão no builder
-builder.Services.AddSingleton<IConfiguration>(new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile($"appsettings.{environment}.json", optional: true)
-    .AddEnvironmentVariables() // Lê as variáveis de ambiente
-    .Build());
-
-// Registra a conexão do Firebird como serviço
-builder.Services.AddScoped<FbConnection>(serviceProvider =>
-    new FbConnection(connectionString));
 
 // Registra o repositório de usuários
 builder.Services.AddScoped<UsuariosRepository>();
@@ -37,11 +22,10 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // Aplica a configuração de CORS
 app.UseHttpsRedirection();
